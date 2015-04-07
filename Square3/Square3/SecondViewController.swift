652ac838fe62cc9fc2a53b3509ec92c7162da298
Harry Shamansky <harry.shamansky@icloud.com>
2015-04-06 20:48:28 -0400

    Initial Commit

---
+++ Square3/Square3/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Square3
+//
+//  Created by Harry Shamansky on 4/6/15.
+//  Copyright (c) 2015 CPSC479. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
